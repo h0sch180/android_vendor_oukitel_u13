594353f6d838c38fbe976691038bc6172be19542
h0sch180 <hoschi@anukis.de>
2019-05-10 11:08:22 +0200

    change drivers

--- U13-vendor-blobs.mk
+++ U13-vendor-blobs.mk
@@ -1,13 +1,13 @@
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/oukitel/U13/lib,system/lib)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/OUKITEL/U13/lib,system/lib)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/oukitel/U13/lib64,system/lib64)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/OUKITEL/U13/lib64,system/lib64)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/oukitel/U13/bin,system/bin)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/OUKITEL/U13/bin,system/bin)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/oukitel/U13/etc,system/etc)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/OUKITEL/U13/etc,system/etc)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/oukitel/U13/usr,system/usr)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/OUKITEL/U13/usr,system/usr)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/oukitel/U13/xbin,system/xbin)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/OUKITEL/U13/xbin,system/xbin)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/oukitel/U13/vendor,system/vendor)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/OUKITEL/U13/vendor,system/vendor)
--- U13-vendor.mk
+++ U13-vendor.mk
@@ -1,1 +1,1 @@
-$(call inherit-product, vendor/oukitel/U13/U13-vendor-blobs.mk)
+$(call inherit-product, vendor/OUKITEL/U13/U13-vendor-blobs.mk)
